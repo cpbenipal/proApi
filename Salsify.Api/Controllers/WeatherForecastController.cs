d6cec47f88155889c31415e673dde0c2f16b812b
mailsforbeni <mailsforbeni@gmail.com>
2022-06-02 00:41:02 +0500

    filter

--- Salsify.Api/Controllers/WeatherForecastController.cs
+++ Salsify.Api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Salsify.Api.Modal;
 
 namespace Salsify.Api.Controllers
 {
@@ -16,15 +19,17 @@ namespace Salsify.Api.Controllers
         [HttpGet(Name = "Auth")]
         public async Task<IActionResult> GetAsync()
         {
+           // List<data> reservationList = new List<data>();
 
-                var requri = "https://app.salsify.com/api/v1/orgs/s-26b1104b-8ce0-44f4-b121-9acb3f679099/products?access_token=DZIf0dDZ-oWwuXwedyhbskz7K0ec6k28udQvjs3158k";
+            var requri = "https://app.salsify.com/api/v1/orgs/s-26b1104b-8ce0-44f4-b121-9acb3f679099/products?access_token=DZIf0dDZ-oWwuXwedyhbskz7K0ec6k28udQvjs3158k&page=1&per_page=50";
 
             using (var httpClient = new HttpClient())
             {
                 using (var response = await httpClient.GetAsync(requri))
                 {
                     var apiResponse = await response.Content.ReadAsStringAsync();
-                    return Ok(apiResponse);
+                    ProductListDTO pd = JsonConvert.DeserializeObject<ProductListDTO>(apiResponse);
+                    return Ok(pd);
                 }
             }
                     
---
+++ Salsify.Api/Modal/ProductDTO.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Salsify.Api.Modal
+{
+    public class modeldata
+    {
+        public string SKU { get; set; }
+        [JsonProperty("salsify:digital_assets")]
+        public List<imagedata> Image { get; set; }
+        [JsonProperty("Product Title")]
+        public string ProductTitle { get; set; }
+        public string Vendor { get; set; }
+    }
+    public class imagedata
+    {
+
+        [JsonProperty("salsify:url")]
+        public string MainImage { get; set; }
+    }
+
+    public class ProductListDTO
+    {
+        public List<modeldata> data { get; set; }
+
+    }
+}
